f30769ff8d535583f5668d078df98ea8ab3d5cdc
jonmudev <jonmudev@gmail.com>
2023-04-30 19:57:14 +0200

    Add User class

---
+++ src/users/domain/User.ts
@@ -0,0 +1,29 @@
+import { AggregateRoot } from '@nestjs/cqrs';
+import { UserCreatedDomainEvent } from './UserCreatedDomainEvent';
+
+export class User extends AggregateRoot {
+  constructor(
+    public readonly id: string,
+    public readonly name: string,
+    public readonly email: string,
+    public readonly password: string,
+    public readonly age: number,
+  ) {
+    //We should implement any encrypting password logic here
+    super();
+  }
+
+  static create(
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    age: number,
+  ): User {
+    const user = new User(id, name, email, password, age);
+
+    user.apply(new UserCreatedDomainEvent(id, name, email, password, age));
+
+    return user;
+  }
+}
