37886317a0f41540721b49731d1131849116d23b
jonmudev <jonmudev@gmail.com>
2023-04-30 20:11:51 +0200

    Fix UserInMemoryRepository dependency

--- src/users/infracstructure/UserInMemoryRepository.ts
+++ src/users/infracstructure/UserInMemoryRepository.ts
@@ -3,7 +3,10 @@ import { User } from '../domain/User';
 
 @Injectable()
 export class UserInMemoryRepository {
-  constructor(private readonly users: User[] = []) {}
+  private readonly users: User[];
+  constructor() {
+    this.users = [];
+  }
 
   persist(user: User): void {
     this.users.push(user);
