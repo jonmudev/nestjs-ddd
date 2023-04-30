b27bc01b2fdade03b018b73d2a6d69e48b164046
jonmudev <jonmudev@gmail.com>
2023-04-30 19:58:09 +0200

    update Users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { CreateUserCommandHandler } from './application/CreateUserCommandHandler';
+import { UserCreator } from './application/UserCreator';
+import { UserInMemoryRepository } from './infracstructure/UserInMemoryRepository';
+
 import { UsersController } from './users.controller';
 
 @Module({
-  imports: [],
+  imports: [CqrsModule],
   controllers: [UsersController],
-  providers: [],
+  providers: [UserCreator, UserInMemoryRepository, CreateUserCommandHandler],
 })
 export class UsersModule {}
